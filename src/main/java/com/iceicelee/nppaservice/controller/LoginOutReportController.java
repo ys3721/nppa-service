6dd6ba0a9747df392c97a54e6f8b6601fda88b78
Yao Shuai <ys3721@hotmail.com>
2021-04-17 16:38:33 +0800

    七月食瓜，八月断壶，九月叔苴，采荼薪樗，食我农夫

--- src/main/java/com/iceicelee/nppaservice/controller/LoginOutReportController.java
+++ src/main/java/com/iceicelee/nppaservice/controller/LoginOutReportController.java
@@ -1,8 +1,28 @@
 package com.iceicelee.nppaservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author: Yao Shuai
  * @date: 2021/4/17 16:33
  */
+@RestController
+@RequestMapping("/game")
 public class LoginOutReportController {
+
+    @RequestMapping(value = "/login.go")
+    public String login(HttpServletRequest req, int userId, String idfa) {
+        String ip = req.getRemoteAddr();
+        return null;
+    }
+
+
+    @RequestMapping(value = "/logout.go")
+    public String logout(HttpServletRequest req, int usreId, String idfa) {
+        String ip = req.getRemoteAddr();
+        return "";
+    }
 }
